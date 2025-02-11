3ea50c771a3c981ea7f17fcfb554b8c852561d82
Anas_Dervin <102409151+Anas040@users.noreply.github.com>
2025-02-11 16:21:49 +0100

    Adding needed image and colors in Color.kt

--- app/src/main/java/com/example/expensetracker/ui/theme/Color.kt
+++ app/src/main/java/com/example/expensetracker/ui/theme/Color.kt
@@ -1,6 +1,8 @@
 package com.example.expensetracker.ui.theme
 
+import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
+
 
 val Purple80 = Color(0xFFD0BCFF)
 val PurpleGrey80 = Color(0xFFCCC2DC)
@@ -9,3 +11,15 @@ val Pink80 = Color(0xFFEFB8C8)
 val Purple40 = Color(0xFF6650a4)
 val PurpleGrey40 = Color(0xFF625b71)
 val Pink40 = Color(0xFF7D5260)
+
+val gradientColors1 =
+    listOf(
+        Color(0xFF429690),
+        Color(0xFF2A7C76)
+    )
+
+val gradientBlue = Brush.linearGradient(
+    colors = gradientColors1,
+    start = androidx.compose.ui.geometry.Offset(0f, 0f), // Top-left corner
+    end = androidx.compose.ui.geometry.Offset(0f, Float.POSITIVE_INFINITY) // Bottom-right corner
+)
